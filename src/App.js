da223eeb377a4d0aa7de6de2f8b0c224de26740c
Josh Stillman <josh.stillman@giantmachines.com>
2018-12-16 12:00:26 -0500

    initialize project and add coming soon page

--- src/App.js
+++ src/App.js
@@ -6,20 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <img src="/electric-neil-logo.png" height="300px" width="300px"  />
+
+          <div>
+            <img src="/under-construction.jpg" height="300px" width="600px"  />
+          </div>
       </div>
     );
   }
