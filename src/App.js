80c8d9d223562df11fb2931088344921631a2e11
Josh Stillman <josh.stillman@giantmachines.com>
2019-02-18 15:22:13 -0500

    update landing page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const baseClass = "App"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <img src="/electric-neil-logo.png" height="300px" width="300px"  />
+        <a href="https://www.facebook.com/ElectricNeil/" target="_blank">
+          <img className={`${baseClass}__logo`} src="/electric-neil-logo2.png" width="350px" />
+        </a>
 
-          <div>
-            <img src="/under-construction.jpg" height="300px" width="600px"  />
+        <div className={`${baseClass}__band-pics`}>
+          <div className={`${baseClass}__band-pics-closeup--landscape`}>
+            <img src="/mario-noir.jpg" height="400px"  />
           </div>
+
+          <div className={`${baseClass}__band-pics-closeup`}>
+            <img src="/josh-noir.jpg" height="400px"  />
+          </div>
+
+          <div className={`${baseClass}__band-pics-closeup`}>
+            <img src="/dan-noir.jpg" height="400px"  />
+          </div>
+
+          <div className={`${baseClass}__band-pics-closeup`}>
+            <img src="/sarah-noir.jpg" height="400px"  />
+          </div>
+        </div>
+
+        <div className={`${baseClass}__links`}>
+          <div className={`${baseClass}__link-item`}>
+            <a href="https://www.facebook.com/ElectricNeil/" target="_blank">
+              <img src="/fb.png" height="64px"  />
+            </a>
+          </div>
+
+          <div className={`${baseClass}__link-item`}>
+            <a href="https://twitter.com/electric_neils" target="_blank">
+              <img src="/twit.png" height="64px"  />
+            </a>
+          </div>
+
+          <div className={`${baseClass}__link-item`}>
+            <a href="mailto:electric.neil.band@gmail.com">
+              <img src="/email.png" height="64px"  />
+            </a>
+          </div>
+        </div>
+
+        <div className={`${baseClass}__footer`}>
+          <div className={`${baseClass}__footer-item`}>
+            <span>
+              Logo by <a href="https://benjaminstillman.com/" target="_blank">Benjamin&nbsp;Stillman</a>
+            </span>
+          </div>
+
+          <div className={`${baseClass}__footer-item`}>
+            Photos by Bernard&nbsp;Bluhm
+          </div>
+        </div>
       </div>
     );
   }
