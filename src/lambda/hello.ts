8d84505e967c08a26c5835c0489f9be83527e9cf
Josh Stillman <josh.stillman@giantmachines.com>
2019-02-18 22:07:09 -0500

    add typescript

---
+++ src/lambda/hello.ts
@@ -0,0 +1,9 @@
+import { Handler, Context, Callback, APIGatewayEvent } from 'aws-lambda';
+
+exports.handler = function(event: APIGatewayEvent, context: Context, callback: Callback) {
+  console.log('queryStringParameters', event.queryStringParameters);
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello, World!' })
+  });
+}
