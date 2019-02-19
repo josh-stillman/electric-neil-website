0fa0d33cc0ea58a2d39d45f65c8f337e0551fe71
Josh Stillman <josh.stillman@giantmachines.com>
2019-02-18 22:52:06 -0500

    add tslint

--- src/lambda/hello.ts
+++ src/lambda/hello.ts
@@ -1,9 +1,9 @@
-import { Handler, Context, Callback, APIGatewayEvent } from 'aws-lambda';
+import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
 
-exports.handler = function(event: APIGatewayEvent, context: Context, callback: Callback) {
+exports.handler = (event: APIGatewayEvent, context: Context, callback: Callback) => {
   console.log('queryStringParameters', event.queryStringParameters);
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ message: process.env.MESSAGE || 'nothin here' })
+    body: JSON.stringify({ message: process.env.MESSAGE || 'nothin here' }),
   });
-}
+};
