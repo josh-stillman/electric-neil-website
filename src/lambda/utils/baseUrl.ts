f41cea91fa3253f4bf99556f1ae5c528f9d9b7f9
Josh Stillman <josh.stillman@giantmachines.com>
2019-04-20 12:13:04 -0400

    add env var switch

--- src/lambda/utils/baseUrl.ts
+++ src/lambda/utils/baseUrl.ts
@@ -1,3 +1,11 @@
-export const getBaseUrl = () => (
-  process.env.DEPLOY_PRIME_URL || process.env.URL || process.env.BASE_URL || ''
-);
+export const getBaseUrl = () => {
+  switch (process.env.CONTEXT) {
+    case 'production':
+      return process.env.URL || '';
+    case 'deploy-preview':
+    case 'branch-deploy':
+      return process.env.DEPLOY_URL || process.env.URL || process.env.BASE_URL || '';
+    default:
+      return process.env.BASE_URL || '';
+  }
+}
